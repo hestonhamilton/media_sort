643e9d27f921c354aab67711bae2e7f396bf93b5
hestonhamilton <hestonshamilton@gmail.com>
2024-02-12 14:42:39 -0700

    Added multiple path parameter for dupe_cleanup.py

--- dupe_cleanup.py
+++ dupe_cleanup.py
@@ -13,20 +13,19 @@ def move_contents_to_new_structure(src_dir, dest_dir):
         dest_item = os.path.join(dest_dir, item)
         shutil.move(src_item, dest_item)
 
-def main(target_path):
+def process_directory(target_path):
     for root, dirs, _ in os.walk(target_path):
         if 'dupe' in dirs:
             dupe_dir = os.path.join(root, 'dupe')
 
-            # Navigate up to get the {file_type} directory
-            file_type_dir = os.path.abspath(os.path.join(root, "../.."))
+            # Navigate up to get the directory where the new duplicates structure will be created
+            target_dir = os.path.abspath(os.path.join(root, "../.."))
 
             # Correctly extract the year and month from the root
-            # The month is the basename of the root, and the year is the basename of its parent directory
             month, year = os.path.basename(root), os.path.basename(os.path.dirname(root))
             
             # Construct the new duplicate directory path
-            new_dupe_dir = os.path.join(file_type_dir, "duplicates", year, month)
+            new_dupe_dir = os.path.join(target_dir, "duplicates", year, month)
             os.makedirs(new_dupe_dir, exist_ok=True)
 
             move_contents_to_new_structure(dupe_dir, new_dupe_dir)
@@ -37,8 +36,12 @@ def main(target_path):
             except OSError:
                 print(f"Directory not empty: {dupe_dir}")
 
+def main(target_paths):
+    for path in target_paths:
+        process_directory(path)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Move "dupe" directories to new "duplicates" structure.')
-    parser.add_argument('target_path', help='Target directory path to clean up')
+    parser.add_argument('target_paths', nargs='+', help='Target directory paths to clean up')
     args = parser.parse_args()
-    main(args.target_path)
+    main(args.target_paths)
